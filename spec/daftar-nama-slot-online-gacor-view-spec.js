8ebddd7bb3d33ef81fa9c971b43aac5c094e86a4
yuzyutyu <suhubet.mail@gmail.com>
2022-07-16 19:56:47 +0700

    page 1 dong

---
+++ spec/daftar-nama-slot-online-gacor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DaftarNamaSlotOnlineGacorView from '../lib/daftar-nama-slot-online-gacor-view';
+
+describe('DaftarNamaSlotOnlineGacorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
